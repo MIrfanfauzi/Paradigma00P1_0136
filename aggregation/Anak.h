9c340c6ccb0801ca77c7a3437089dfdf20d6563e
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-20 11:19:28 +0700

    membuat class anak

--- aggregation/Anak.h
+++ aggregation/Anak.h
@@ -4,3 +4,14 @@
 using namespace std;
 #include <iostream>
 
+class anak {
+public:
+	string nama;
+	anak(string pNama) : nama(pNama) {
+		cout << "Anak \"" << nama << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\"tidak ada\n";
+	}
+};
+#endif
