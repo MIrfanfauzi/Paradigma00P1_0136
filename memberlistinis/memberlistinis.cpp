e673c2e31d702ae1f8e52baffed6d83e2b139567
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-20 10:38:11 +0700

    membuat member function destructor

--- memberlistinis/memberlistinis.cpp
+++ memberlistinis/memberlistinis.cpp
@@ -13,7 +13,11 @@ public:
         //definisi
     }
 
-    
+    ~mahasiswa() {
+        cout << "Id = " << id << endl;
+        cout << "Nama =  " << nama << endl;
+        cout << "Nilai = " << nilai << endl;
+    }
 
 };
 
