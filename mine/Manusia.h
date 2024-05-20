b5b8f33a9b7e115467e43bb91902f7fa084d77a3
Irfan Fauzi <irfanfauziyk@gmail.com>
2024-05-20 10:58:36 +0700

    membuat class manusia

--- mine/Manusia.h
+++ mine/Manusia.h
@@ -3,5 +3,18 @@
 using namespace std;
 #include <iostream>
 
+class manusia {
+public:
+	string name;
+	jantung varJantung;
 
+	manusia(string pName)
+		: name(pName) {
+		cout << name << "hidup\n";
+	}
+	~manusia() {
+		cout << name << "mati\n";
+	}
+};
+#endif
 
